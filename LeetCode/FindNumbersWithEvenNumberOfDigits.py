26eeba70762ab387fe92346eb5ba50c4b6847f2a
nabeelahmedjh <abdulnabeel40@gmail.com>
2022-07-25 12:19:35 +0500

    Used a formula to find the digits in a number

--- LeetCode/FindNumbersWithEvenNumberOfDigits.py
+++ LeetCode/FindNumbersWithEvenNumberOfDigits.py
@@ -1,11 +1,25 @@
+import math
+
+
 class Solution:
     def findNumbers(self, nums: list[int]) -> int:
         even = 0
         for i in nums:
-            if len(str(i)) % 2 == 0:
+            digits = self.digits(i)
+
+            if self.isEven(digits):
                 even += 1
 
         return even
+
+    def isEven(self, num):
+        return (num % 2 == 0)
+
+    def digits(self, num):
+        if num < 0:
+            num *= -1
+
+        return (math.floor(math.log10(num)) + 1)
 
 
 def main():
